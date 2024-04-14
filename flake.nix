2cbb516fc3a81db0cb51d1185ba07d6a79e45f52
nix <rwstauffer72@gmail.com>
2024-04-14 17:10:40 -0400

    make it nixy

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A java flake with csc 116 dev tools";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+    };
+
+    crane.url = "github:ipetkov/crane";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    crane,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      devToolchain = fenix.packages."${system}".stable; # NOTE stable
+      craneLib = crane.lib.${system};
+    in {
+      # For `nix build` & `nix run`:
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+        nativeBuildInputs = [pkgs.pkg-config pkgs.sqlite];
+      };
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (devToolchain.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          rust-analyzer
+        ];
+      };
+    });
+}
