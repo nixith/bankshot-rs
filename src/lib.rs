e69cb75cf0377bc17d1583ea95c833a3cd2c4c12
nix <rwstauffer72@gmail.com>
2024-04-14 21:41:15 -0400

    add session storage for authorization

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 use axum::{
     http::StatusCode,
-    response::{IntoResponse, Json, Redirect},
+    response::{Html, IntoResponse, Json, Redirect},
 };
 use rusqlite::{params, Connection, Result, ToSql};
 use serde::{Deserialize, Serialize};
 use std::sync::OnceLock;
+use tokio::{signal, task::AbortHandle};
+use tower_sessions::Session;
 use tracing::info;
 use tracing::{error, warn};
 
 const DB_NAME: &str = "ducks.db";
+const AUTH_KEY: &str = "IsAuthed";
 
 // functions to hold html store index.html at runtime
 pub fn index_html() -> &'static str {
@@ -27,26 +30,51 @@ pub fn auth_html() -> &'static str {
     LLM_HTML.get_or_init(|| std::fs::read_to_string("templates/auth.html").unwrap())
 }
 
-pub async fn authed() -> bool {
-    true // TODO: implement
+// serializable struct for if a user is authed
+#[derive(Default, Deserialize, Serialize)]
+struct AuthSession(bool);
+
+pub async fn is_authed(session: Session) -> bool {
+    // return what is heled by auth key, default to false if anything happens
+    session
+        .get(AUTH_KEY)
+        .await
+        .unwrap_or(Some(false))
+        .unwrap_or(false)
 }
 
-// the input to our `create_user` handler
+pub async fn return_llm_page(session: Session) -> impl IntoResponse {
+    if is_authed(session).await {
+        Html(bill_html())
+    } else {
+        Html(auth_html())
+    }
+}
+
+// the input to our login form handler
 #[derive(Deserialize)]
 pub struct LoginForm {
     username: String,
     password: String,
 }
 
-pub async fn login(Json(payload): Json<LoginForm>) -> (StatusCode, Json<LoginResponse>) {
+pub async fn login(
+    session: Session, // current session
+    Json(payload): Json<LoginForm>,
+) -> (StatusCode, Json<LoginResponse>) {
     //TODO: Store session cookie
     match auth(payload.username, payload.password).await {
-        Ok(true) => (
-            StatusCode::OK,
-            Json(LoginResponse {
-                message: "Logged In".to_string(),
-            }),
-        ),
+        Ok(true) => {
+            // store session cookie
+            session.insert(AUTH_KEY, true).await.unwrap();
+            //respond with json
+            (
+                StatusCode::OK,
+                Json(LoginResponse {
+                    message: "Logged In".to_string(),
+                }),
+            )
+        }
         Ok(false) => (
             StatusCode::FORBIDDEN,
             Json(LoginResponse {
@@ -92,14 +120,14 @@ pub fn setup_db() -> Result<()> {
             password: "password1".to_string(),
         },
         User {
-            id: 0,
-            name: "Swan1".to_string(),
-            password: "password1".to_string(),
+            id: 1,
+            name: "Swan2".to_string(),
+            password: "password2".to_string(),
         },
         User {
-            id: 0,
-            name: "Swan1".to_string(),
-            password: "password1".to_string(),
+            id: 2,
+            name: "Swan3".to_string(),
+            password: "password3".to_string(),
         },
     ];
     for me in users {
@@ -109,7 +137,10 @@ pub fn setup_db() -> Result<()> {
         )?;
     }
     _ = conn.execute("COMMIT", ()).map_err(|err| {
-        warn!("Unable to create BankShot DB on Initialization");
+        warn!(
+            "Unable to create BankShot DB on Initialization. Reason: {}",
+            err
+        );
         err
     });
     info!("Database created sucessfully!");
@@ -135,5 +166,35 @@ pub async fn auth(username: String, password: String) -> Result<bool, String> {
         Ok(Some(_)) => Ok(true),
         Ok(None) => Ok(false),
         Err(s) => Err(s.to_string()),
+    }
+}
+
+pub async fn shutdown_signal(deletion_task_abort_handle: AbortHandle) {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            info!("Shutting down Session Store");
+            deletion_task_abort_handle.abort()
+        },
+        _ = terminate => {
+
+            info!("Shutting down Session Store");
+            deletion_task_abort_handle.abort() },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use askama_axum::Result;
 use axum::{
     response::{Html, Redirect},
     routing::{get, post},
@@ -7,11 +8,15 @@ use ctf::*;
 
 use tower_http::services::ServeDir;
 
+use tower_sessions::{session_store::ExpiredDeletion, Expiry, SessionManagerLayer};
+use tower_sessions_rusqlite_store::tokio_rusqlite::Connection;
+use tower_sessions_rusqlite_store::RusqliteStore;
+
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 // actual http server, everything here just specifies which functions handle what
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initialize tracing
     tracing_subscriber::registry()
         .with(
@@ -21,33 +26,61 @@ async fn main() {
         .init();
     // intialize DB
     setup_db().unwrap(); // we panic on db initializaiton failure for now
+
+    // SESSION MANAGEMENT
+    // make session connection
+    //
+    // initialize session key store
+    //
+    // This can be a DB on disk, memory for now since we're just testing
+    let conn = Connection::open_in_memory().await.unwrap();
+    let session_store = RusqliteStore::new(conn);
+    session_store.migrate().await?;
+
+    // check every minute, delete cookies that are an hour old
+    let deletion_task = tokio::task::spawn(
+        session_store
+            .clone()
+            .continuously_delete_expired(tokio::time::Duration::from_secs(5)),
+    );
+
+    // initialize sesstion_store
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_expiry(Expiry::OnInactivity(
+            tower_sessions::cookie::time::Duration::hours(1),
+        ))
+        .with_signed(tower_sessions::cookie::Key::generate());
+
+    // Ensure we use a shutdown signal to abort the deletion task.
+
     let assets_path = std::env::current_dir().unwrap();
     // build our application with a route
     let app = Router::new()
         // GET routes - serve html
         .route("/", get(|| async { Redirect::permanent("/bankshot") }))
         .route("/bankshot", get(|| async { Html(index_html()) }))
-        .route(
-            "/bankshot/llm",
-            get(|| async {
-                if authed().await {
-                    Html(bill_html())
-                } else {
-                    Html(auth_html())
-                }
-            }),
-        )
+        .route("/bankshot/llm", get(return_llm_page))
         // POST routes - respond with json
         .route("/login", post(login))
         // extra Tower services - things like serving directories
         .nest_service(
             "/static",
             ServeDir::new(format!("{}/static", assets_path.to_str().unwrap())),
-        );
+        )
+        // add session store layer
+        .layer(session_layer);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("starting server");
-    axum::serve(listener, app).await.unwrap();
-    info!("webserver started");
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal(deletion_task.abort_handle()))
+        .await
+        .unwrap();
+
+    // cleanup session store;
+    deletion_task.await??;
+    info!("webserver stopped");
+
+    Ok(())
 }
